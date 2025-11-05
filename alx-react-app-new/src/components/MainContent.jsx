90f941adf9d64c77a7576c05b72ce0f8d411e613
khayfire <kayode354@gmail.com>
2025-11-05 17:41:09 +0100

    reactr

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+  return (
+    <main style={{ backgroundColor: '#eef', padding: '20px', textAlign: 'center' }}>
+      <p>Welcome to my React app! Here I share my favorite cities and personal info.</p>
+    </main>
+  );
+}
+
+export default MainContent;
