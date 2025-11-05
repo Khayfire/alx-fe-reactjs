4da591c38f8ed0b293fd40a73932e9f21991aa60
khayfire <kayode354@gmail.com>
2025-11-05 18:25:33 +0100

    prop drills and context

---
+++ my-company/src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './Navbar';
+import Home from './Home';
+import About from './About';
+import Services from './Services';
+import Contact from './Contact';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path='/Footer' element={<Footer />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
