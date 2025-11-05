a9e4113e931baa5950041aebceca0d86058443a9
khayfire <kayode354@gmail.com>
2025-11-05 17:42:56 +0100

    reactr

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -2,6 +2,7 @@ function MainContent() {
   return (
     <main style={{ backgroundColor: '#eef', padding: '20px', textAlign: 'center' }}>
       <p>Welcome to my React app! Here I share my favorite cities and personal info.</p>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
     </main>
   );
 }
